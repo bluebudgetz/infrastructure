1dcecf229c870e5bdc427e8d2ae8b8349fd88741
Arik Kfir <arik@kfirs.com>
2020-04-25 17:40:30 +0300

    Add "gate" repository

--- github.tf
+++ github.tf
@@ -2,6 +2,10 @@ variable "github_token" {
   type = string
 }
 
+variable "dockerhub_access_token" {
+  type = string
+}
+
 provider "github" {
   token        = var.github_token
   organization = "bluebudgetz"
@@ -20,4 +24,25 @@ resource "github_repository" "infrastructure" {
   delete_branch_on_merge = true
   has_downloads          = false
   topics                 = ["infrastructure"]
+}
+
+resource "github_repository" "gate" {
+  name                   = "gate"
+  description            = "Bluebudgetz API gateway"
+  private                = false
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = false
+  topics                 = ["api", "go", "golang"]
+}
+
+resource "github_actions_secret" "gate_dockerhub_access_token" {
+  repository       = "gate"
+  secret_name      = "DOCKERHUB_ACCESS_TOKEN"
+  plaintext_value  = var.dockerhub_access_token
 }
